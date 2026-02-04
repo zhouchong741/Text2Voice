a5a149f2c82f736e1c2e1f7cb61cf3d033d7f774
OVOPARK\zhouchong <zhouchong@ovopark.com>
2026-02-04 11:07:34 +0800

    feat: Implement PWA functionality by adding a manifest, icon, and service worker.

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'voice-loop-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './style.css',
+    './script.js',
+    './manifest.json',
+    './icon.svg'
+];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((response) => response || fetch(e.request))
+    );
+});
